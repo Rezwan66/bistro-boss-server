e9e3df33187f6a9a18871b0fc425140b2414adaa
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-22 00:04:16 +0600

    created get api to get admin stats

--- index.js
+++ index.js
@@ -210,7 +210,6 @@ async function run() {
             const result = await paymentCollection.find(query).toArray();
             res.send(result);
         })
-
         app.post('/payments', async (req, res) => {
             const payment = req.body;
             const paymentResult = await paymentCollection.insertOne(payment);
@@ -225,6 +224,31 @@ async function run() {
             const deleteResult = await cartCollection.deleteMany(query);
 
             res.send({ paymentResult, deleteResult });
+        })
+
+        // ADMIN: stats or analytics
+        app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
+            const users = await userCollection.estimatedDocumentCount();
+            const menuItems = await menuCollection.estimatedDocumentCount();
+            const orders = await paymentCollection.estimatedDocumentCount();
+            // total revenue- this is not the best way
+            // const payments = await paymentCollection.find().toArray();
+            // const revenue = payments.reduce((total, item) => total + item.price, 0);
+            // Total revenue by mongoDB's aggregation framework
+            const result = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalRevenue: {
+                            $sum: '$price'
+                        }
+                    }
+                }
+            ]).toArray();
+            // console.log(result);
+            const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+            res.send({ users, menuItems, orders, revenue })
         })
 
     } finally {
